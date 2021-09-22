55ac3ea4cf46ed8e6319009c8b902e67712061bc
Artur Naberezhnyi <artur.naberegniy@gmail.com>
2021-09-22 16:54:39 +0300

    Test log4j

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -20,6 +20,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import DAO.*;
 import entities.*;
 import controllers.*;
@@ -27,9 +31,15 @@ import controllers.*;
 @WebServlet("/test")
 public class Test extends HttpServlet {
   
+	static final Logger rootLogger = LogManager.getRootLogger();
+	
 	private static final long serialVersionUID = -1822377043264836486L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hello server");
+		
+		rootLogger.info("Hello server");
+		
 		ServletContext servletContext = getServletContext();
 		RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/Test.jsp");
 		requestDispatcher.forward(request, response);
